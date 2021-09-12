7350bc7312e5e72f74b5e67a73c0ef60c65697ff
Nishtha Gupta <70621712+nishthagupta055@users.noreply.github.com>
2021-09-12 06:06:09 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,33 +1,29 @@
 #include<stdio.h>
-const int maxN=1e5;
-int memo[100000];
-void fibbonacci(int index, int n , int count)
-{
-    memo[index] = memo[index - 1] + memo[index - 2];
-    if(((memo[index]&1)^1))
-    {
-    	printf("%d ",memo[index]);
-    	count++;
-    }
-    if(count >= n){
-       return;
-    }
-  
-  
-    fibbonacci(index + 1 , n ,count);
+
+
+int evenFibonacci(int n){
+  if(n == 0 || n == 1)
+    return n;
+
+  return evenFibonacci(n - 1) + evenFibonacci(n - 2);
 }
 
-void fib(int n) {
-    memo[1] = 1;
-    memo[2] = 1;
-    fibbonacci(3,n,0);
-}
+int main() {
+  // The number of even Fibonacci numbers to print:
+  int n = 5;
 
-int main()
-{
-    int n ;
-    printf("Enter n = ");
-    scanf("%d",&n);
-    fib(n);
+  int x;
+  for(int i = 0; n > 0; i++){
+    x = evenFibonacci(i);
+    
+    // Check if the number returned is even:
+    if(x % 2 == 0){
+      cout << x << " ";
+      n--;
+    }
+  }
+  
+  return 0;
+}
 
 }
